3e5ede9c0ccbbb6c0a30b47fb94d620553dacf3e
byoon97 <byoon5397@gmail.com>
2020-07-20 11:35:27 -0400

    yay

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,40 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    sushis: [],
+    money: 100,
+    eaten : [],
+    currentDisplayed : 0
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      this.setState({sushis : data})
+    }) 
+  }
+
+  renderFour = () => {
+    return this.state.sushis.slice(this.state.currentDisplayed, this.state.currentDisplayed+4)
+  }
+
+  moreSushi = (event) => {
+    let newCurrentDisplayed = this.state.currentDisplayed + 4
+
+    this.setState({currentDisplayed : newCurrentDisplayed})
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushis={this.renderFour()}
+                        more={this.moreSushi} 
+                        eaten={this.state.eaten} 
+                      />
+        <Table money={this.state.money}
+              />
       </div>
     );
   }
